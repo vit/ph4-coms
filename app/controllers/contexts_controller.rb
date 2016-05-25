7a121a162ad9cdf1bb0e769cbc4f293c3350cda2
vit2 <shiegin@gmail.com>
2016-05-25 12:59:08 +0000

    fixed two small issues

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -74,6 +74,6 @@ class ContextsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def context_params
 #      params.require(:context).permit(:title, :desription, :slug, :type, :user_id)
-      params.require(:context).permit(:title, :desription, :slug, :type)
+      params.require(:context).permit(:title, :description, :slug, :type)
     end
 end
